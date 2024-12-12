d9ff9179af5259d3a6fac734b382cb3427b763a1
kamil4921 <kamil.j4921@gmail.com>
2024-12-12 19:53:14 +0100

    Changes to asynchronous communication to clean up the code

--- src/Work360.Services.Employee.Infrastructure/Services/MessageBroker.cs
+++ src/Work360.Services.Employee.Infrastructure/Services/MessageBroker.cs
@@ -8,21 +8,18 @@ namespace Work360.Services.Employee.Infrastructure.Services;
 
 public class MessageBroker : IMessageBroker
 {
-    private const string connectionString = "Endpoint=sb://localhost:5672/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RootManageSharedAccessKeyValue;UseDevelopmentEmulator=true;";
-    public async Task PublishAsync(params INotification[] events)
-    {
-        var client = new ServiceBusClient(connectionString);
-        var sender = client.CreateSender("employee-topic");
-        var message = new ServiceBusMessage(Newtonsoft.Json.JsonConvert.SerializeObject(events));
-
-        await sender.SendMessageAsync(message);
-    }
+    private const string ConnectionString = "Endpoint=sb://localhost:5672/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=RootManageSharedAccessKeyValue;UseDevelopmentEmulator=true;";
+    public Task PublishAsync(params INotification[] events) => PublishAsync(events.AsEnumerable());
 
     public async Task PublishAsync(IEnumerable<INotification> events)
     {
+        var client = new ServiceBusClient(ConnectionString);
+        var sender = client.CreateSender("employee-topic");
+        var message = new ServiceBusMessage(Newtonsoft.Json.JsonConvert.SerializeObject(events));
+        
         foreach (var @event in events)
         {
-            await PublishAsync(@event);
+            await sender.SendMessageAsync(message);
         }
     }
 }
